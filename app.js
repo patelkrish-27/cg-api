30304b09f957c40209c88657aa87185b01e065a3
PATEL KRISH <125797284+patelkrish-27@users.noreply.github.com>
2024-12-05 21:41:43 +0530

    initail , with cohen only

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// Endpoint to download a file
+app.get("/cohen", (req, res) => {
+  const filePath = path.join(__dirname, "files", "COHEN.CPP"); // Path to your file
+  res.download(filePath, "COHEN.CPP", (err) => {
+    if (err) {
+      console.error("Error while sending the file:", err);
+      res.status(500).send("Error downloading the file");
+    }
+  });
+});
+
+app.get("/dda", (req, res) => {
+  const filePath = path.join(__dirname, "files", "COHEN.CPP"); // Path to your file
+  res.download(filePath, "COHEN.CPP", (err) => {
+    if (err) {
+      console.error("Error while sending the file:", err);
+      res.status(500).send("Error downloading the file");
+    }
+  });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
