8f6fbf5ce5377b233e046f61b4a29abeb4aa2453
Krish Patel <krispatel27x@gmail.com>
2024-12-05 23:36:45 +0530

    changes file names and made dynamic route

--- app.js
+++ app.js
@@ -4,29 +4,9 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
-// Endpoint to download a file
-app.get("/cohen", (req, res) => {
-  const filePath = path.join(__dirname, "files", "COHEN.CPP"); // Path to your file
-  res.download(filePath, "COHEN.CPP", (err) => {
-    if (err) {
-      console.error("Error while sending the file:", err);
-      res.status(500).send("Error downloading the file");
-    }
-  });
-});
 
-app.get("/dda", (req, res) => {
-  const filePath = path.join(__dirname, "files", "DDA.CPP"); // Path to your file
-  res.download(filePath, "dda.CPP", (err) => {
-    if (err) {
-      console.error("Error while sending the file:", err);
-      res.status(500).send("Error downloading the file");
-    }
-  });
-});
-
-app.get("/bresen", (req, res) => {
-  const filePath = path.join(__dirname, "files", "BRESENHAM.CPP"); // Path to your file
+app.get("/:name", (req, res) => {
+  const filePath = path.join(__dirname, "files", `${req.params.name}.CPP`); // Path to your file
   res.download(filePath, "bresen.CPP", (err) => {
     if (err) {
       console.error("Error while sending the file:", err);
